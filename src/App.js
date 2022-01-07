57f574867fb65dad0ec217800f5178d6fdff5bc7
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2022-01-07 23:31:54 +0100

    Made the pokemons into buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+export const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonDetails, setPokemonDetails] = useState([]);
 
-function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await response.json();
+      setPokemons(data.results);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchPokemonInfo = async () => {
+      for (let pokemon of pokemons) {
+        const pokemonResponse = await fetch(pokemon.url);
+        const pokemonData = await pokemonResponse.json();
+        pokemon.info = pokemonData;
+      }
+
+      setPokemonDetails(pokemons);
+    };
+
+    fetchPokemonInfo();
+  }, [pokemons]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pokemonDetails.map((pokemon) => {
+        return (
+          <div class="row">
+            <div class="col-6 col-sm-4">
+              <div class="card p-0 overflow-hidden h-100 shadow">
+                <button type="button" class="btn btn-outline-light">
+                  <div class="card-body text-center bg-dark text-white">
+                    <div key={pokemon.url}>
+                      <h3 class="card-title">{pokemon.name}</h3>
+                      <img src={pokemon.info.sprites.front_default} alt="" />
+                    </div>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
